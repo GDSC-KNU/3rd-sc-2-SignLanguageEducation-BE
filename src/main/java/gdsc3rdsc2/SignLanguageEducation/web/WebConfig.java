e32450fb78b3c86c0afa8a99d55a482a74df710e
Gyumin Sim <gyu2781@gmail.com>
2024-02-01 05:38:30 +0900

    Establish : CORS 설정

---
+++ src/main/java/gdsc3rdsc2/SignLanguageEducation/web/WebConfig.java
@@ -0,0 +1,16 @@
+package gdsc3rdsc2.SignLanguageEducation.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+}
