350f58fd6d8a52c00ead427c48a713b69eb9a15b
akuby21 <39649594+akuby21@users.noreply.github.com>
2024-01-31 16:04:47 +0900

    Feat : User Join (wip)
    Implementing User join, login(wip)
    Jwt and DB are going to be added

---
+++ src/test/java/com/gdsc/rdsc2/SignLanguageEducation/SignLanguageEducationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdsc.rdsc2.SignLanguageEducation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignLanguageEducationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
