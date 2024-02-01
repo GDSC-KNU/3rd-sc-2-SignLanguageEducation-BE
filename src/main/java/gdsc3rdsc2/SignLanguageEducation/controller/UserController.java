06eeb726cb979fffcfdcda5b17c3900983e41e05
Jiwon Ryu <39649594+akuby21@users.noreply.github.com>
2024-02-01 21:31:06 +0900

    Feat : Login

--- src/main/java/gdsc3rdsc2/SignLanguageEducation/controller/UserController.java
+++ src/main/java/gdsc3rdsc2/SignLanguageEducation/controller/UserController.java
@@ -1,8 +1,10 @@
 package gdsc3rdsc2.SignLanguageEducation.controller;
 
 import gdsc3rdsc2.SignLanguageEducation.domain.dto.UserJoinRequest;
+import gdsc3rdsc2.SignLanguageEducation.domain.dto.UserLoginRequest;
 import gdsc3rdsc2.SignLanguageEducation.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,8 +16,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/user")
 public class UserController {
     private final UserService userService;
+
     @PostMapping("/join")
     public ResponseEntity<String> join(@RequestBody UserJoinRequest dto){
+        userService.join(dto.getUserName(),dto.getPassword());
+
         return ResponseEntity.ok().body("success");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> log(@RequestBody UserLoginRequest dto){
+        String token = userService.login(dto.getUserName(),dto.getPassword());
+        return ResponseEntity.ok().body(token);
     }
 }
