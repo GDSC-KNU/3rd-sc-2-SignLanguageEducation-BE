c887ac98caa719a8cb9070ac96f55155b5b80023
Jiwon Ryu <39649594+akuby21@users.noreply.github.com>
2024-02-01 21:28:14 +0900

    Feat : Jwt Tokens while login process

--- src/main/java/gdsc3rdsc2/SignLanguageEducation/service/UserService.java
+++ src/main/java/gdsc3rdsc2/SignLanguageEducation/service/UserService.java
@@ -2,7 +2,9 @@ package gdsc3rdsc2.SignLanguageEducation.service;
 
 import gdsc3rdsc2.SignLanguageEducation.domain.User;
 import gdsc3rdsc2.SignLanguageEducation.repository.UserRepository;
+import gdsc3rdsc2.SignLanguageEducation.util.JwtTokenUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,11 +12,16 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    @Value("${jwt.token.secret}")
+    private String key;
+
+    private Long expireTimeMs = 1000 * 60 * 60 * 24L;
+
     public String join(String userName, String password){
 
         userRepository.findByUserName(userName)
                 .ifPresent(user -> {
-                    throw new IllegalArgumentException("이미 존재하는 회원입니다.");
+                    throw new RuntimeException("이미 존재하는 회원입니다.");
                 });
 
         User user = User.builder()
@@ -23,5 +30,16 @@ public class UserService {
                 .build();
         userRepository.save(user);
         return "SUCCESS";
+    }
+
+    public String login(String userName, String password){
+        User selectedUser = userRepository.findByUserName(userName)
+                .orElseThrow(() -> new RuntimeException("해당하는 회원이 존재하지 않습니다."));
+
+        if(selectedUser.getPassword() == password){
+            throw new RuntimeException("비밀번호를 잘못 입력했습니다.");
+        }
+
+        return JwtTokenUtil.createToken(selectedUser.getUserName(),key,expireTimeMs);
     }
 }
