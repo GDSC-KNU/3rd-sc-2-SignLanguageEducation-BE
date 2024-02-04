0d065337207bb3f7aaa126b8de85f7de0a8b11b1
Gyumin Sim <gyu2781@gmail.com>
2024-02-05 05:09:33 +0900

    Feat : 컨트롤러 레이어 - 관심사로 문장 찾기, 스크립트 목록 조회, 스크립트 상세 조회 api 작성

--- src/main/java/gdsc3rdsc2/SignLanguageEducation/controller/StudyController.java
+++ src/main/java/gdsc3rdsc2/SignLanguageEducation/controller/StudyController.java
@@ -1,13 +1,12 @@
 package gdsc3rdsc2.SignLanguageEducation.controller;
 
-import gdsc3rdsc2.SignLanguageEducation.domain.dto.UserJoinRequest;
-import gdsc3rdsc2.SignLanguageEducation.service.UserService;
 import gdsc3rdsc2.SignLanguageEducation.service.StudyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.support.ResourceRegion;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -16,10 +15,26 @@ import java.util.Optional;
 public class StudyController {
     private final StudyService studyService;
 
+    @GetMapping("/sentence/{concern}")
+    public Map<Long, String> findSentence(@PathVariable String concern){
+        return studyService.findSentence(concern);
+    }
+
     @PostMapping("/sentence")
-    public Map<String, Long> choiceSentence(@RequestBody String sentence){
+    public Map<String, Long> selectSentence(@RequestBody String sentence){
         return studyService.choiceSentence(sentence);
     }
+
+    @GetMapping("/script")
+    public Map<Long, String> getScriptList(){
+        return studyService.getScriptList();
+    }
+
+    @GetMapping("/script/{scriptId}")
+    public List<String> getScript(@PathVariable Long scriptId){
+        return studyService.getScript(scriptId);
+    }
+
 
     @GetMapping("/video/{videoId}")
     public ResponseEntity<ResourceRegion> getVideo(@PathVariable Long videoId, @RequestHeader HttpHeaders headers){
