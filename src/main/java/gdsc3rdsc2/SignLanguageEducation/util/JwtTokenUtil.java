4e0a237058e07bfbb841acf6c0b5c10d64c1500e
akuby21 <39649594+akuby21@users.noreply.github.com>
2024-02-05 05:39:11 +0900

    Feat : adding interceptors
    api 사용 인가를 위한 interceptor 도입

--- src/main/java/gdsc3rdsc2/SignLanguageEducation/util/JwtTokenUtil.java
+++ src/main/java/gdsc3rdsc2/SignLanguageEducation/util/JwtTokenUtil.java
@@ -3,10 +3,29 @@ package gdsc3rdsc2.SignLanguageEducation.util;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.Date;
 
+
 public class JwtTokenUtil {
+
+    public static String getUserName(String token, String secretKey){
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().get("userName",String.class);
+    }
+
+    public static boolean isValidToken(String token,String key) throws Exception {
+        try {
+            Claims claims = Jwts.parser()
+                    .setSigningKey(key)
+                    .parseClaimsJws(token)
+                    .getBody();
+            return true;
+        } catch(Exception e) {
+            throw new Exception("유효하지 않은 토큰입니다.");
+        }
+    }
+
     public static String createToken(String userName, String key, long expireTimeMs){
         Claims claims = Jwts.claims();
         claims.put("userName",userName);
@@ -15,6 +34,7 @@ public class JwtTokenUtil {
                 .setClaims(claims)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + expireTimeMs))
+                .claim("userName",userName)
                 .signWith(SignatureAlgorithm.HS256,key)
                 .compact();
     }
