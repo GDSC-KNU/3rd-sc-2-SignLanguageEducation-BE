fab9a666cd8a154bbbf21b6170ac77337123989d
Gyumin Sim <gyu2781@gmail.com>
2024-02-05 05:08:36 +0900

    Feat : 스크립트 레포지토리와 스크립트 목록 조회 메소드 작성

---
+++ src/main/java/gdsc3rdsc2/SignLanguageEducation/repository/ScriptRepository.java
@@ -0,0 +1,13 @@
+package gdsc3rdsc2.SignLanguageEducation.repository;
+
+import gdsc3rdsc2.SignLanguageEducation.domain.Script;
+import gdsc3rdsc2.SignLanguageEducation.domain.projection.ScriptProjection;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ScriptRepository extends JpaRepository<Script, Long> {
+    List<ScriptProjection> findAllScriptProjection();
+}
