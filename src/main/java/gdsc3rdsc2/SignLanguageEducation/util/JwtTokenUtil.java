6c49156b579af1ff24abab3cec3827deb580847d
akuby21 <39649594+akuby21@users.noreply.github.com>
2024-02-13 04:39:55 +0900

    Fix : fix Jwt token decode error
    Jwt 파싱시 시크릿 키의 문자열 그대로 넘겨 인증이 제대로 안되던 문제 해결

--- src/main/java/gdsc3rdsc2/SignLanguageEducation/util/JwtTokenUtil.java
+++ src/main/java/gdsc3rdsc2/SignLanguageEducation/util/JwtTokenUtil.java
@@ -1,41 +1,61 @@
 package gdsc3rdsc2.SignLanguageEducation.util;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
 
+@Component
 public class JwtTokenUtil {
 
     public static String getUserName(String token, String secretKey){
-        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().get("userName",String.class);
+        return Jwts.parser().setSigningKey(secretKey.getBytes()).parseClaimsJws(token).getBody().get("userName",String.class);
     }
 
     public static boolean isValidToken(String token,String key) throws Exception {
         try {
             Claims claims = Jwts.parser()
-                    .setSigningKey(key)
+                    .setSigningKey(key.getBytes())
                     .parseClaimsJws(token)
                     .getBody();
             return true;
-        } catch(Exception e) {
+        } catch(ExpiredJwtException e) {
+            throw new Exception("만료된 토큰입니다.");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
             throw new Exception("유효하지 않은 토큰입니다.");
         }
     }
 
-    public static String createToken(String userName, String key, long expireTimeMs){
+    public static String createAccessToken(String userName, String key, long expireTimeMs){
         Claims claims = Jwts.claims();
         claims.put("userName",userName);
-
-        return Jwts.builder()
+        String accessToken = Jwts.builder()
                 .setClaims(claims)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + expireTimeMs))
                 .claim("userName",userName)
-                .signWith(SignatureAlgorithm.HS256,key)
+                .signWith(SignatureAlgorithm.HS256, key.getBytes())
                 .compact();
+
+        return accessToken;
+    }
+
+    public static String createRefreshToken(String userName, String key, Long refreshTokenExpireTimeMs) {
+        Claims claims = Jwts.claims();
+        claims.put("userName",userName);
+        String refreshToken = Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + refreshTokenExpireTimeMs))
+                .claim("userName",userName)
+                .signWith(SignatureAlgorithm.HS256, key.getBytes())
+                .compact();
+
+        return refreshToken;
     }
 }
