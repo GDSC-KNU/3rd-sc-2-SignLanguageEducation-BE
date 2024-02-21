ecf5e718963520da4b13ff81c5f3b9ea27a6c0c3
Gyumin Sim <gyu2781@gmail.com>
2024-02-22 04:24:04 +0900

    Remove : video 엔티티와 레포지토리 삭제

--- src/main/java/gdsc3rdsc2/SignLanguageEducation/repository/VideoRepository.java
+++
@@ -1,9 +0,0 @@
-package gdsc3rdsc2.SignLanguageEducation.repository;
-
-import gdsc3rdsc2.SignLanguageEducation.domain.Video;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VideoRepository extends JpaRepository<Video, Long> {
-}
